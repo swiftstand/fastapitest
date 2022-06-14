a11985a68f904c68f28cb77c4664438ed24a8cd6
hamed swift <swifthmd@gmail.com>
2022-06-14 15:29:30 -0700

    only

--- main.py
+++ main.py
@@ -10,18 +10,35 @@ import json
 
 class Query(ObjectType):
     product_list = None
-    get_products = List(ProductType,{"id":Int(), "category":String()})
-    async def resolve_get_products(self,info,id=None, category= None):
+    products_details = List(ProductType,{"id":Int()})
+    products_by_category = List(ProductType,{"category":String()})
+    products_overview = List(ProductType)
+    # async def resolve_products(self,info):
+    #     with open('./items.json') as products:
+    #         product_list = json.load(products)
+    #     return product_list
+
+    async def resolve_products_by_category(self,info,category= None):
+        with open('./items.json') as products:
+            product_list = json.load(products)
+        if (category):
+            return [item for item in product_list if item["category"] == category]
+
+        return "Item with provided category doesn't exist"
+
+    
+    async def resolve_products_details(self,info,id=None):
         with open('./items.json') as products:
             product_list = json.load(products)
         if (id):
             for item in product_list:
                 if item["id"] == id:
                     return [item]
+            return "Item with provided id doesn't exist"
 
-        if (category):
-            return [item for item in product_list if item["category"] == category]
-
+    async def resolve_products_overview(self,info):
+        with open('./items.json') as products:
+            product_list = json.load(products)
         return product_list
 
 app = FastAPI()
